807bb26c939de5787ebba17fa08c95011475af37
kylebaker <kylebaker@Kyles-MacBook-Pro.local>
2015-03-04 19:24:08 -0700

    sodalicious

---
+++ js/index.js
@@ -0,0 +1,6 @@
+	smoothScroll.init({
+	speed: 1200, // Integer. How fast to complete the scroll in milliseconds
+	easing: 'easeInOutCubic', // Easing pattern to use
+	updateURL: false, 
+	offset: 0, 
+});

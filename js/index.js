d9d3f6997c0cba24a03fd1a302c1ec833c680ce6
kylebaker <kylethomasbaker@gmail.com>
2019-08-14 22:24:35 -0600

    Restarting Website

--- js/index.js
+++
@@ -1,6 +0,0 @@
-	smoothScroll.init({
-	speed: 1200, // Integer. How fast to complete the scroll in milliseconds
-	easing: 'easeInOutCubic', // Easing pattern to use
-	updateURL: false, 
-	offset: 0, 
-});
